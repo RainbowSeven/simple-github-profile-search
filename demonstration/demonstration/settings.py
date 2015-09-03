cd74810480359839eaf35415a4b5a38baef95147
RainbowSeven <thezurg07@gmail.com>
2015-09-03 22:08:04 +0100

    Remove TEMPLATE_DIRS directive from project configuration

--- demonstration/demonstration/settings.py
+++ demonstration/demonstration/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,7 +57,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'TEMPLATE_DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,4 +105,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-

a98587a95a3ad372915e6a4b191a152c9d070362
RainbowSeven <thezurg07@gmail.com>
2015-09-03 13:07:32 +0100

    Walkthrough backend integration of Github's API

---
+++ demonstration/app/views.py
@@ -0,0 +1,32 @@
+import json
+import requests
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hello World!')
+
+
+def test(request):
+    return HttpResponse('Second link here as always!')
+
+
+def profile(request):
+    jsonList = []
+    req = requests.get('https://api.github.com/users/DrkSephy')
+    jsonList.append(json.loads(req.content))
+    parsedData = []
+    userData = {}
+    for data in jsonList:
+        userData['name'] = data['name']
+        userData['blog'] = data['blog']
+        userData['email'] = data['email']
+        userData['public_gists'] = data['public_gists']
+        userData['public_repos'] = data['public_repos']
+        userData['avatar_url'] = data['avatar_url']
+        userData['followers'] = data['followers']
+        userData['following'] = data['following']
+
+    parsedData.append(userData)
+    return HttpResponse(parsedData)
